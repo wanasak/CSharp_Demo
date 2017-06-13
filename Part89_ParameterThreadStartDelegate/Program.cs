955d16e21343ab1767b4b5cc0647cd6c5548e12c
wanasak <u510610433@gmail.com>
2017-06-13 18:28:38 +0700

    add more demo

---
+++ Part89_ParameterThreadStartDelegate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Part89_ParameterThreadStartDelegate
+{
+    class Number
+    {
+        public static void PrintNumbers(object target)
+        {
+            if (int.TryParse(target.ToString(), out int number))
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            object target = Console.ReadLine();
+
+            // Option 1
+            // Create an parameterized thread start
+            //ParameterizedThreadStart parameterThread = new ParameterizedThreadStart(Number.PrintNumbers);
+            //Thread thread = new Thread(parameterThread);
+
+            // Option 2
+            // The compiler can implicit convert automatically
+            Thread thread = new Thread(Number.PrintNumbers);
+
+            thread.Start(target);
+        }
+    }
+}
---
+++ Part90_TypeSafeParameterThreadStartDelegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Part90_TypeSafeParameterThreadStartDelegate
+{
+    class Number
+    {
+        int _target;
+        public Number(int target)
+        {
+            this._target = target;
+        }
+        public void PrintNumbers()
+        {
+            for (int i = 1; i <= _target; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int target = Convert.ToInt32(Console.ReadLine());
+
+            Number number = new Number(target);
+
+            Thread thread = new Thread(new ThreadStart(number.PrintNumbers));
+            thread.Start();
+        }
+    }
+}
